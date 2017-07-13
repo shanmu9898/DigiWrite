dc22c289e8c0afd9e30f8df068934f16c836cef6
shanmu9898 <shanmu9898@gmail.com>
2017-07-13 16:00:29 +0530

    Permissions are fixed
    the runtime permissions are now fixed

--- app/src/main/java/com/mukesh/android/digiwritetesting2/MainActivity.java
+++ app/src/main/java/com/mukesh/android/digiwritetesting2/MainActivity.java
@@ -10,10 +10,15 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.scanlibrary.ScanActivity;
@@ -23,57 +28,56 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button scandocumentbutton;
-    Button opengallerybutton;
+    ImageButton scandocumentbutton;
+    ImageButton  opengallerybutton;
     int REQUEST_CODE = 99;
+    public  static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scandocumentbutton = (Button)findViewById(R.id.scan_document_button);
-        opengallerybutton = (Button)findViewById(R.id.open_gallery_button);
+        scandocumentbutton = (ImageButton )findViewById(R.id.scan_document_button);
+        opengallerybutton = (ImageButton )findViewById(R.id.open_gallery_button);
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission();
+        }
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant
+
+
+
+
+
+
         scandocumentbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(intent);
             }
         });
 
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA},2
-                );
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant
-
-                return;
-            }
-        }
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},3
-                );
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant
-
-                return;
-            }
-        }
-
         opengallerybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Intent i = new Intent(Intent.ACTION_PICK,
-                  //      android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                //      android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
                 //final int ACTIVITY_SELECT_IMAGE = 1234;
                 //startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},3
+                        );
 
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant
+
+
+                    }
+                }
 
                 int preference = ScanConstants.OPEN_MEDIA;
                 Intent intent = new Intent(MainActivity.this, ScanActivity.class);
@@ -81,6 +85,86 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
+
+
+
+
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) + ContextCompat
+                .checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (MainActivity.this, Manifest.permission.CAMERA)) {
+
+                Snackbar.make(MainActivity.this.findViewById(android.R.id.content),
+                        "Please Grant Permissions to upload profile photo",
+                        Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if (Build.VERSION.SDK_INT >= 23) {
+                                    requestPermissions(
+                                            new String[]{Manifest.permission
+                                                    .READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                                            PERMISSIONS_MULTIPLE_REQUEST);
+                                }
+
+                            }
+                        }).show();
+            } else {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    requestPermissions(
+                            new String[]{Manifest.permission
+                                    .READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                            PERMISSIONS_MULTIPLE_REQUEST);
+                }
+
+
+            }
+        } else {
+            // write your logic code if permission already granted
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case PERMISSIONS_MULTIPLE_REQUEST:
+                if (grantResults.length > 0) {
+                    boolean cameraPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean readExternalFile = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if(cameraPermission && readExternalFile)
+                    {
+                        // write your logic here
+                    } else {
+                        Snackbar.make(MainActivity.this.findViewById(android.R.id.content),
+                                "Please Grant Permissions to upload profile photo",
+                                Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                                new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if (Build.VERSION.SDK_INT >= 23) {
+                                            requestPermissions(
+                                                    new String[]{Manifest.permission
+                                                            .READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                                                    PERMISSIONS_MULTIPLE_REQUEST);
+                                        }
+                                    }
+                                }).show();
+                    }
+                }
+                break;
+        }
     }
 
     @Override
@@ -112,9 +196,9 @@ public class MainActivity extends AppCompatActivity {
             Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
             String stringuri = uri.toString();
 
-                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
-                intent.putExtra("Imagefilepath",stringuri);
-                startActivity(intent);
+            Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+            intent.putExtra("Imagefilepath",stringuri);
+            startActivity(intent);
 
 
         }
